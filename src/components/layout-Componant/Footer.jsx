538e050d82131354829bcb03bda99d85509aa337
KevinPieplu <pieplu.kevin@gmail.com>
2025-07-08 12:19:57 +0200

    feat: composant footer

--- src/components/layout-Componant/Footer.jsx
+++ src/components/layout-Componant/Footer.jsx
@@ -1,5 +1,11 @@
+import kasaLogoW from '@/assets/logoKasa_w.svg'
+import '@/styles/footer.scss'
+
 export function Footer(){
     return(
-        <p>Footer</p>
+        <footer>
+            <img src={kasaLogoW} alt="logo kasa"/>
+            <p>© 2020 Kasa. All rights reserved</p>
+        </footer>
     )
 }
