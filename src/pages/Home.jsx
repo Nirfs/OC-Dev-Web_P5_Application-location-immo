2f56548f5b5f5835f8685c14d20355860ce6cabd
KevinPieplu <pieplu.kevin@gmail.com>
2025-07-08 14:29:39 +0200

    feat: card style et page home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,16 @@
+import { useLoaderData } from "react-router-dom";
+import { Card } from "../components/Card";
+import '@/styles/Pages/home.scss'
+
 export function Home() {
-  return <h1>Accueil</h1>;
+  const data = useLoaderData()
+  console.log(data)
+
+  return (
+    <div className="card-container">
+      {data.map((post) => (
+        <Card key={post.id} src={post.cover} alt={post.title} text={post.title} navLink={`/logement/${post.id}`}/>
+      ))}
+    </div>
+  )
 }
