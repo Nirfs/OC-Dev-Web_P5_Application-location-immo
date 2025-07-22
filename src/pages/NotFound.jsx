44dd72165a0ad9dfe685bb4cb3e5f14055b71324
KevinPieplu <pieplu.kevin@gmail.com>
2025-07-22 15:25:39 +0200

    style: modification de style pour correspondre au plus a la maquette

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -5,7 +5,7 @@ export function NotFound(){
     return (
         <>
             <div className="error-Container">
-                <h1> 404</h1>
+                <h2> 404</h2>
                 <p>Oups! La page que vous demandez n'existe pas.</p>
                 <NavLink to="/">Retourner sur la page d’accueil</NavLink>
             </div>
